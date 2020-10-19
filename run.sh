78b26e6e3054e220cf4a603285f3f341398b3811
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2020-10-19 16:18:15 -0400

    refactor: print repo url when run

--- run.sh
+++ run.sh
@@ -4,8 +4,11 @@ set -e
 cd /docs
 
 if [ -d .git ]; then
+    CURR_REMOTE=$(git remote show origin | grep 'Fetch URL' | sed -E 's/(.+: )//g')
+    echo "Existing repo $CURR_REMOTE";
     git pull;
 else
+    echo "DOC_REPO=$DOC_REPO";
     git clone "$DOC_REPO" .;
 fi;
 
